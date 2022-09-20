8b96e330e2faf328105511bdfe6148b42b442a7e
cfg-gk <111849520+cfg-gk@users.noreply.github.com>
2022-09-20 12:29:09 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,24 +3,49 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"time"
 )
 
-func IntegerToBinary(n int) string {
-	return strconv.FormatInt(int64(n), 2)
+func genAlphabets(c chan<- string, n string) {
+	//num := 65
+	//var alphabet string
+	// for i := 65; i <= 90; i++ {
+	// 	alphabet += string(i)
+	// }
+	// switch{
+	// 	case c <- n:
+	// 	n++
+	// }
+
+	//return string(alphabet)
+	c <- n
+}
+func genNumbers(c chan<- string, n string) {
+	// var number string
+	// for i := 1; i <= 26; i++ {
+	// 	number += strconv.Itoa(i)
+	// }
+	//return number
+	c <- n
+}
+func PrintChannel(c <-chan string) { //<-chan  as were are reading
+	for {
+		msg := <-c
+		fmt.Print(msg)
+	}
 }
 func main() {
-	num := 123
-	bitFormat := IntegerToBinary(num)
-	//fmt.Println("Binary representation:", IntegerToBinary(num), len(a))
-	var bitSlice []string
-	var count int
-	for _, val := range bitFormat {
-		bitSlice = append(bitSlice, string(val))
-		if string(val) == "1" {
-			count++
-		}
+	c := make(chan string)
+	num := 65
+	x := 1
+	for num < 91 {
+		go genAlphabets(c, string(num))
+		go genNumbers(c, strconv.Itoa(x))
+		go PrintChannel(c)
+		num++
+		x++
 
+		time.Sleep(1 * time.Millisecond)
 	}
-	bitSlice = append(bitSlice, strconv.Itoa(count))
-	fmt.Println("val:", bitSlice)
+
 }
