1e83d781cfaba1de40ec2e3f4eefc449583612ca
cfg-gk <111849520+cfg-gk@users.noreply.github.com>
2022-09-21 09:28:38 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,74 +2,39 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"time"
 )
 
-type user struct {
-	userid string
-	pwd    string
+func genNumbers(ch chan int, n int) {
+	ch <- n
 }
 
-var option string
+func primeNumberValidate(ch chan int) {
+	count := 0
+	number := <-ch
 
-func loginfnc() {
-	var operation string
+	for i := 2; i <= number/2; i++ {
 
-	fmt.Println("Login")
-	fmt.Printf("Enter the user name:")
-	fmt.Scan(&curUser.userid)
-	fmt.Printf("Enter the password:")
-	fmt.Scan(&curUser.pwd)
-	if curUser.userid == "Test" && curUser.pwd == "test" {
-	opt:
-		fmt.Println("Please select the option to proceed:")
-		fmt.Println("\nl -> WithDraw\n2 -> Deposit\n5 -> Balance\n4 -> Quit")
-		fmt.Scan(&operation)
-
-		switch operation {
-		case "1":
-
-			fmt.Println("Please collect the cash")
-			goto opt
-		case "2":
-			fmt.Println("Amount deposited")
-			goto opt
-		case "3":
-			fmt.Println("Account Balance is XXXXX")
-			goto opt
-		case "4":
-			fmt.Println("Thank you for banking with us")
-			os.Exit(4)
+		if number%i == 0 {
+			count++
 		}
 
-	} else {
-		main()
 	}
-}
-func createAcct() {
-	newUser := user{}
-	fmt.Println("Enter the user name")
-	fmt.Scan(&newUser.userid)
-	fmt.Println("Enter the password")
-	fmt.Scan(&newUser.pwd)
-	fmt.Println("Acct created")
-	main()
-}
+	if count == 0 && number != 1 {
+		fmt.Println(number, "-> Prime Number")
+	} else {
+		fmt.Println(number, "-> Not Prime Number")
+	}
 
-var curUser user
+}
 
 func main() {
-	fmt.Println("Hi! Welcome to Mr. Rohit ATM Machine!\n\nPlease select an option from the menu below:")
-	fmt.Println("\ni -> Login\nc -> Create New Account\nq -> Quit")
-	fmt.Scan(&option)
-	switch option {
-	case "i":
-		loginfnc()
-	case "c":
-		createAcct()
-	case "q":
-		fmt.Println("Thank you for Banking with us..")
-		os.Exit(4)
+	num := make(chan int)
+	loop := 1
+	for loop <= 100 {
+		go genNumbers(num, loop)
+		go primeNumberValidate(num)
+		loop++
+		time.Sleep(2 * time.Millisecond)
 	}
-
 }
