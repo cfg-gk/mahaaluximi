d28f40cbba027fa3a6342ebe17a851abe1d633bc
cfg-gk <111849520+cfg-gk@users.noreply.github.com>
2022-09-21 11:39:16 +0530

    Add files via upload
    Assignment6

--- main.go
+++ main.go
@@ -1,75 +1,28 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
-type user struct {
-	userid string
-	pwd    string
-}
-
-var option string
-
-func loginfnc() {
-	var operation string
-
-	fmt.Println("Login")
-	fmt.Printf("Enter the user name:")
-	fmt.Scan(&curUser.userid)
-	fmt.Printf("Enter the password:")
-	fmt.Scan(&curUser.pwd)
-	if curUser.userid == "Test" && curUser.pwd == "test" {
-	opt:
-		fmt.Println("Please select the option to proceed:")
-		fmt.Println("\nl -> WithDraw\n2 -> Deposit\n5 -> Balance\n4 -> Quit")
-		fmt.Scan(&operation)
-
-		switch operation {
-		case "1":
-
-			fmt.Println("Please collect the cash")
-			goto opt
-		case "2":
-			fmt.Println("Amount deposited")
-			goto opt
-		case "3":
-			fmt.Println("Account Balance is XXXXX")
-			goto opt
-		case "4":
-			fmt.Println("Thank you for banking with us")
-			os.Exit(4)
-		}
-
-	} else {
-		main()
-	}
-}
-func createAcct() {
-	newUser := user{}
-	fmt.Println("Enter the user name")
-	fmt.Scan(&newUser.userid)
-	fmt.Println("Enter the password")
-	fmt.Scan(&newUser.pwd)
-	fmt.Println("Acct created")
-	main()
-}
-
-var curUser user
+import "fmt"
 
 func main() {
-	fmt.Println("Hi! Welcome to Mr. Rohit ATM Machine!\n\nPlease select an option from the menu below:")
-	fmt.Println("\ni -> Login\nc -> Create New Account\nq -> Quit")
-	fmt.Scan(&option)
-	switch option {
-	case "i":
-		loginfnc()
-	case "c":
-		createAcct()
-	case "q":
-		fmt.Println("Thank you for Banking with us..")
-		os.Exit(4)
+	numList := []int{5, 2, 8, 6, 4, 9}
+	target := 22
+	var consumedList []int
+	for index, val := range numList {
+		if len(consumedList) > 0 {
+			break
+		}
+		for i := index + 1; i < len(numList); i++ {
+			if val+numList[i] == target {
+				consumedList = append(consumedList, index)
+				consumedList = append(consumedList, i)
+				break
+			}
+		}
+
 	}
 
+	if len(consumedList) > 0 {
+		fmt.Println("Target found on summations of", consumedList)
+	} else {
+		fmt.Println("Target can't be found on summation of numbers")
+	}
 }
